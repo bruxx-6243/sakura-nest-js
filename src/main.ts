6cb2041d7364d0f4b091770f27b8562d967ef0d9
brx-nbif <blondeau.nbif@mail.ru>
2025-08-13 23:28:02 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT = process.env.PORT ?? 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
+}
+
+bootstrap()
+  .then(() => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+  })
+  .catch((error: unknown) => {
+    console.error(error);
+  });
---
+++ src/property/property.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('property')
+export class PropertyController {
+  @Get()
+  findAll() {
+    return 'All properties';
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id', ParseIntPipe, new DefaultValuePipe(1)) id: number,
+    @Query('sort', ParseBoolPipe, new DefaultValuePipe(false)) sort: boolean,
+    @Query('limit', ParseIntPipe, new DefaultValuePipe(10)) limit: number = 10,
+  ) {
+    console.log(sort, limit, id);
+
+    return `Property ${id} ${sort} ${limit}`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create() {
+    return 'Create property';
+  }
+}
